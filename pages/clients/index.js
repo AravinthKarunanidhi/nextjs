fa6f8d037c2d316fc88eb7f88b1c5ee59f0e6740
AravinthKarunanidhi <aravinthinangu@gmail.com>
2022-05-29 15:50:50 +0530

    router chagnes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
 function ClientPage(){
+    const clients = [
+        {id:'max',name:'Max'},
+        {id:'milli', name:'Millinium'}
+    ]
     return(
         <div>
             <h1>The Client page !!</h1>
+            <ul>
+                {
+                    clients.map(client=><li key={client.id}>
+                        <Link href={{
+                            pathname:'/clients/[id]',
+                            query :{id:client.id}
+                        }}>{client.name}</Link>
+                    </li>)
+                }
+            </ul>
         </div>
     )
 }
